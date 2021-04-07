dc67655c851743f467d64d44fc043ab2e19ab582
Cristian Roman <cristianroman@Cristians-MacBook-Pro.local>
2021-04-07 14:05:03 -0700

    Additional information

--- sampleScript_Apr7.R
+++ sampleScript_Apr7.R
@@ -1,3 +1,4 @@
+##Packages
 library(pbapply)
 library(deming)
 library(pbmcapply)
@@ -6,7 +7,22 @@ library(deming)
 library(R2jags)
 library(data.table)
 
+#Read the relevant scripts in the functions folder
 sapply(list.files('Functions', full.names = T), source)
+
+##The dataset with the default structure
 data<-read.csv('Data/SampleData.csv')
-simulateAll(data, replicates=5, samples=100, generations=1000, isMixed=F)
+
+##Run the seven regression models using 5 replicates per model and sampling 100
+##observations per replicate. The number of generations for the Bayesian models
+##is set to 1,000 but we generally need 20.000. The following line will ignore
+##material identity (i.e. the mixed model is not included)
+simulateAll(data, replicates=5, samples=100, generations=1000)
+
+##If the user wants to analyze also the mixed model, use isMixed=T
+
 simulateAll(data, replicates=5, samples=100, generations=1000, isMixed=T)
+
+
+##Thanks for all your patience, Hannah! Hope that this is much more organized!
+
