babc2381ac2b1d9ed3794ecef5f459bcbcb81a9a
Cristian Roman <cristianroman@Cristians-MacBook-Pro.local>
2021-04-07 14:02:02 -0700

    extra line

--- sampleScript_Apr7.R
+++ sampleScript_Apr7.R
@@ -7,7 +7,6 @@ library(R2jags)
 library(data.table)
 
 sapply(list.files('Functions', full.names = T), source)
-
 data<-read.csv('Data/SampleData.csv')
 simulateAll(data, replicates=5, samples=100, generations=1000, isMixed=F)
 simulateAll(data, replicates=5, samples=100, generations=1000, isMixed=T)
