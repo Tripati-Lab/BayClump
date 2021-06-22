3168e98abe45be9369cfb47408477ffe998fda36
Cristian Roman <cristianroman@Cristians-MacBook-Pro.local>
2021-06-21 20:57:57 -0700

    updates clumpipe

--- sampleScriptPipelinePredictionsJune21.R
+++ sampleScriptPipelinePredictionsJune21.R
@@ -13,6 +13,9 @@ library(plyr)
 library(clumpedr) ##devtools::install_github("isoverse/clumpedr", ref = "dev")
 library(data.table)
 library(doBy)
+library(readxl)
+library(ggthemr)
+ggthemr('pale', layout = 'scientific', spacing = 2, type = 'inner')
 
 #Read the relevant scripts in the functions folder
 sapply(list.files('Functions', full.names = T), source)
@@ -26,44 +29,34 @@ PipCriteria <- read_excel("~/MEGA/Projects/1_ClumpedIsotopes/PredSummary_Jun18.x
                                 sheet = "selected")
 
 
-##Modestou
-targetD47<-c(0.714,
-             0.715,
-             0.723,
-             0.714,
-             0.713,
-             0.708,
-             0.712,
-             0.706,
-             0.705,
-             0.709,
-             0.705,
-             0.706,
-             0.695,
-             0.71)
-error_targetD47<-c(0.0067,
-                   0.0067,
-                   0.0066,
-                   0.0067,
-                   0.0067,
-                   0.007,
-                   0.0064,
-                   0.0065,
-                   0.007,
-                   0.0071,
-                   0.0068,
-                   0.0066,
-                   0.0071,
-                   0.0067)
+##Dataset
+Miocene_Datasets <- read_excel("~/Downloads/Miocene Datasets.xlsx", 
+                               sheet = "Modestou2020 (Averages)")
 
+##Modestou------
+targetD47<-Miocene_Datasets$`D47 (ICDES90)`
+error_targetD47<-Miocene_Datasets$seD47
 
+#Updated frame
+PredUpdatedFrame<-predictTcNonBayes(data=cbind(targetD47,error_targetD47), 
+                  slope=0.0449, 
+                  slpcnf=0.001, 
+                  intercept=0.167, 
+                  intcnf=0.01)
+
+PredOriginalFrame<-predictTcNonBayes(data=cbind(Miocene_Datasets$`D47 (Bernasconi Ref. Frame)`,error_targetD47), 
+                                    slope=0.0449, 
+                                    slpcnf=0.001, 
+                                    intercept=0.167, 
+                                    intcnf=0.01)
 
 infTempBayesian<-clumpipe(calData=calData,
          PipCriteria=PipCriteria, 
          targetD47=targetD47, 
          error_targetD47=error_targetD47, 
-         nrep=1000,
+         nrep=100,
          BayesianOnly=T)
+
 
 infTempBest<-clumpipe(calData=calData,
                   PipCriteria=PipCriteria, 
@@ -72,5 +65,115 @@ infTempBest<-clumpipe(calData=calData,
                   nrep=1000,
                   BayesianOnly=F)
 
+
+Data_complete <-rbindlist(list(cbind(group="OriginalFrame",PredOriginalFrame),
+               cbind(group="NewFrame",PredUpdatedFrame),
+               cbind(group="clumpipe",   infTempBest[,c(19,20,21:23)]),
+               cbind(group="Bayesian",  infTempBayesian[,c(1,2,21:23)])), fill = T)
+
+Data_complete$Age<- rep(Miocene_Datasets$Age, nrow(Data_complete)/nrow(Miocene_Datasets))
+Data_complete$type<-ifelse(is.na(Data_complete$type), "Parameter Uncertainty", Data_complete$type)
+
+write.csv(Data_complete, 'Modestou.predictions.csv')
+
+
+ModestouPlot<-ggplot()+
+  geom_ribbon(data=Data_complete[Data_complete$type== 'Parameter Uncertainty' & Data_complete$group != 'OriginalFrame' ,], aes(x=Age, y=Tc,ymin=lwr, ymax=upr,fill=group, color = group), alpha=0.1, linetype='dotted')+
+  geom_ribbon(data=NULL,aes(ymin=infTempBest[,'Tc'], ymax=infTempBayesian[,'Tc'], x=
+                              Miocene_Datasets$Age), fill='#FFDB58', alpha=0.9)+
+  geom_line(data=Data_complete[Data_complete$type== 'Parameter Uncertainty' & Data_complete$group != 'OriginalFrame' ,], 
+            aes(x=Age, y=Tc, color=group), size=1)+
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
+  
+pdf('Modestou.pdf', 7,5)
+print(ModestouPlot)
+dev.off()
+
+
+ModestouPlotOriginal<-ggplot(data=Data_complete[Data_complete$group %in% c('OriginalFrame','NewFrame') ,])+
+  geom_ribbon(aes(x=Age, y=Tc,ymin=lwr, ymax=upr,fill=type, color = type), alpha=0.1, linetype='dotted')+
+  geom_line(aes(x=Age, y=Tc, color=type), size=1)+
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10))+
+  facet_wrap(~group)
+
+
+pdf('Modestou_IgnoreParameters.pdf', 10,5)
+print(ModestouPlotOriginal)
+dev.off()
+
+
+###XXXXXX--------
+Miocene_Datasets <- read_excel("~/Downloads/Miocene Datasets.xlsx", 
+                               sheet = "Leutert2020 (Averages)")
+
+##Leutert2020------
+targetD47<-Miocene_Datasets$`D47 (ICDES90)`
+error_targetD47<-Miocene_Datasets$seD47
+
+#Updated frame
+PredUpdatedFrame<-predictTcNonBayes(data=cbind(targetD47,error_targetD47), 
+                                    slope=0.0449, 
+                                    slpcnf=0.001, 
+                                    intercept=0.167, 
+                                    intcnf=0.01)
+
+PredOriginalFrame<-predictTcNonBayes(data=cbind(Miocene_Datasets$`D47 (Bernasconi Ref. Frame)`,error_targetD47), 
+                                     slope=0.0449, 
+                                     slpcnf=0.001, 
+                                     intercept=0.167, 
+                                     intcnf=0.01)
+
+infTempBayesian<-clumpipe(calData=calData,
+                          PipCriteria=PipCriteria, 
+                          targetD47=targetD47, 
+                          error_targetD47=error_targetD47, 
+                          nrep=100,
+                          BayesianOnly=T)
+
+
+infTempBest<-clumpipe(calData=calData,
+                      PipCriteria=PipCriteria, 
+                      targetD47=targetD47, 
+                      error_targetD47=error_targetD47, 
+                      nrep=1000,
+                      BayesianOnly=F)
+
+
+Data_complete <-rbindlist(list(cbind(group="OriginalFrame",PredOriginalFrame),
+                               cbind(group="NewFrame",PredUpdatedFrame),
+                               cbind(group="clumpipe",   infTempBest[,c(19,20,21:23)]),
+                               cbind(group="Bayesian",  infTempBayesian[,c(1,2,21:23)])), fill = T)
+
+Data_complete$Age<- rep(Miocene_Datasets$Age, nrow(Data_complete)/nrow(Miocene_Datasets))
+Data_complete$type<-ifelse(is.na(Data_complete$type), "Parameter Uncertainty", Data_complete$type)
+
+
+write.csv(Data_complete, 'Leutert2020.predictions.csv')
+
+
+
+ModestouPlot<-ggplot()+
+  geom_ribbon(data=Data_complete[Data_complete$type== 'Parameter Uncertainty' & Data_complete$group != 'OriginalFrame' ,], aes(x=Age, y=Tc,ymin=lwr, ymax=upr,fill=group, color = group), alpha=0.1, linetype='dotted')+
+  geom_ribbon(data=NULL,aes(ymin=infTempBest[,'Tc'], ymax=infTempBayesian[,'Tc'], x=
+                              Miocene_Datasets$Age), fill='#FFDB58', alpha=0.9)+
+  geom_line(data=Data_complete[Data_complete$type== 'Parameter Uncertainty' & Data_complete$group != 'OriginalFrame' ,], 
+            aes(x=Age, y=Tc, color=group), size=1)+
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
+
+pdf('Leutert2020.pdf', 7,5)
+print(ModestouPlot)
+dev.off()
+
+
+ModestouPlotOriginal<-ggplot(data=Data_complete[Data_complete$group %in% c('OriginalFrame','NewFrame') ,])+
+  geom_ribbon(aes(x=Age, y=Tc,ymin=lwr, ymax=upr,fill=type, color = type), alpha=0.1, linetype='dotted')+
+  geom_line(aes(x=Age, y=Tc, color=type), size=1)+
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10))+
+  facet_wrap(~group)
+
+
+pdf('Leutert2020_IgnoreParameters.pdf', 10,5)
+print(ModestouPlotOriginal)
+dev.off()
 
 
