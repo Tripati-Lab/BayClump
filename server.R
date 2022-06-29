93e1348dbd62deeec5faa81c43a4942ae4da223f
Cristian Román-Palacios <cromanpa94@gmail.com>
2022-06-29 09:01:14 -0700

    preprint updated

--- server.R
+++ server.R
@@ -1456,7 +1456,7 @@ server <- function(input, output, session) {
   # Manuscript tab
   
   output$msframe <- renderUI({
-    tags$iframe(style="height:600px; width:100%", src="Manuscript/essoar.10507995.1.pdf")
+    tags$iframe(style="height:600px; width:100%", src="essoar.10507995.2.pdf")
   })
   
 }
