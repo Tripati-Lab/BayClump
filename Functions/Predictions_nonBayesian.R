a59c53edcfdec30380ab6ac094f1e099468f7917
Cristian Román-Palacios <cromanpa94@gmail.com>
2022-09-15 16:51:07 -0700

    Adding the "simple inversion interval" from McClelland et al.'s paper

--- Functions/Predictions_nonBayesian.R
+++ Functions/Predictions_nonBayesian.R
@@ -1,55 +1,53 @@
 #' This function performs temp reconstruction (10^6/T^2 with T in K) for
 #' multiple replicates of the same target.
 #' 
-#' @param targety independent measurements of D47
-#' @param calData Calibration data with the following columns: D47, T2, 
-#'                TempError, D47error
-#' @param model string: lm, wlm, Deming, York
+#' @param calData Calibration dataset
+#' @param recData Reconstruction dataset
+#' @param obCal Data.frame summarizing the distribution of slopes and intercepts
+#' @clumpedClassic Whether the classic calibration or simple inversion should be conducted
 
-predictTc <<- function(calData, 
-                       targety, 
-                       obCal){
-  calData <<- calData
-  obCal <<- obCal
-  std <- function(x) sd(x)/sqrt(length(x))
+predictTc <<- function(calData,
+                       recData,
+                       obCal,
+                       clumpedClassic = TRUE){
   
-  mod <<- stats::nls(formula = D47  ~ a + b1*T2,
-                          data = calData, 
-                          start = list(a = mean(obCal$alpha),b1 = mean(obCal$beta)),
-                          lower = c(a = (mean(obCal$alpha)-std(obCal$alpha)*1.96),
-                                    b1 = (mean(obCal$beta)-std(obCal$beta)*1.96)),
-                          upper = c(a = (mean(obCal$alpha)+std(obCal$alpha)*1.96),
-                                    b1 = (mean(obCal$beta)+std(obCal$beta)*1.96)),
-                          algorithm = "port") 
-      
-      estimate <<- investr::invest(mod, y0 = targety,
-                                   interval = "percentile", 
-                                   seed = 3,  
-                                   nsim =1,
-                                   extendInt="yes", 
-                                   progress=F, 
-                                   lower=-100,
-                                   upper=100)
-
-    preds <- cbind.data.frame(D47 = mean( targety ), 
-                              D47se = sd(targety)/sqrt(length(targety)),
-                              temp = estimate$estimate, 
-                              se = estimate$se, 
-                              lwr = estimate$lower, 
-                              upr = estimate$upper
-    )
   
-      predsUp <- sqrt(10^6/(preds$temp+preds$se))-273.15
-      predsPoint <- sqrt(10^6/preds$temp)-273.15
-      
-      preds <- cbind.data.frame(D47 = preds$D47, 
-                                D47se = preds$D47se, 
-                                temp = predsPoint, 
-                                se = predsPoint - predsUp, 
-                                lwr = predsPoint - ((predsPoint-predsUp)*1.96), 
-                                upr = predsPoint + ((predsPoint-predsUp)*1.96)
-                                )
-
+  if(clumpedClassic){
+    
+    temp <- sqrt((mean(obCal$beta) * 10 ^ 6) / 
+                   (recData$D47 - mean(obCal$alpha))) - 273.15
+    temp_SE <- sqrt((mean(obCal$beta) * 10 ^ 6) / 
+                      (recData$D47 + recData$D47error - mean(obCal$alpha))) - 273.15
+    se = temp-temp_SE
+    recTempS <- cbind.data.frame(Sample = recData$Sample,
+                                 D47 = recData$D47,
+                                 D47error = recData$D47error,
+                                 meanTemp = temp, 
+                                 Temp_L = temp - 1.96 * se, 
+                                 Temp_H = temp + 1.96 * se)
+    
+  }else{
+    
+    #McClelland, H. L., Halevy, I., Wolf‐Gladrow, D. A., Evans, D., & Bradley, A. S. (2021). Statistical uncertainty in paleoclimate proxy reconstructions. Geophysical Research Letters, 48(15), e2021GL092773.
+    
+    X_new_hat  <- (recData$D47 - mean(obCal$alpha)) / mean(obCal$beta)
+    
+    Cqts   <- c(0.025, 0.975)
+    Tval   <- qt(Cqts, df = (nrow(calData)-2)) 
+    Yhat   <-  mean(obCal$alpha) + calData$Temperature*mean(obCal$beta) 
+    ysd    <- sd(calData$D47 - Yhat)
+    Xe1    <- (ysd * Tval) / mean(obCal$beta)
+    Xe1L   <- sqrt(10^6/ (X_new_hat + Xe1[1])) - 273.15
+    Xe1U   <- sqrt(10^6/(X_new_hat + Xe1[2])) - 273.15 
+    
+    cbind.data.frame(Sample = recData$Sample, 
+                     D47 = recData$D47, 
+                     D47error = recData$D47error, 
+                     meanTemp = sqrt(10^6/X_new_hat)-273.15, 
+                     Temp_L = Xe1U, 
+                     Temp_H = Xe1L)
+  }
+  
 }
 
 
